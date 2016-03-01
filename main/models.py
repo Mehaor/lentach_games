9b18fb8278a6cfcd5c6c750bdca840563a2dc450
mk <mihasiksli@yandex.ru>
2016-03-01 22:39:23 +0300

    model Game update

--- main/models.py
+++ main/models.py
@@ -8,9 +8,10 @@ from lentach_games import settings
 class Game(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=50, unique=True)
-    description = models.TextField()
+    description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     cover_image = models.ImageField(null=True, blank=True)
+    is_published = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.slug
--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('_index.html', {})
+    return render_to_response('_index.html', {'game_slug': 'penguin'})
 # Create your views here.
